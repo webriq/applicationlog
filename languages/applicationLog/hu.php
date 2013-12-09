2149708b426a9899191f9773baad19cdd2f6e70b
Gridguyz Dev Platform <dev-platform@gridguyz.com>
2013-12-09 02:22:17 +0000

    Translations updated

---
+++ languages/applicationLog/hu.php
@@ -0,0 +1,12 @@
+<?php
+return array (
+  'applicationLog.anonymous' => 'Anonymus',
+  'applicationLog.eventType.user-login' => 'Belépés',
+  'applicationLog.eventType.user-logout' => 'Kilépés',
+  'applicationLog.list.column.eventType.title' => 'Eseménytípus',
+  'applicationLog.list.column.extra.title' => 'Extra információ',
+  'applicationLog.list.column.loggedUserName.title' => 'Felhasználónév',
+  'applicationLog.list.column.priority.title' => 'Prioritás',
+  'applicationLog.list.column.timestamp.title' => 'Időbélyeg',
+  'applicationLog.priority.3' => 'Hiba',
+);
